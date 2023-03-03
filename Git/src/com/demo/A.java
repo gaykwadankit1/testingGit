fbe8af01ae139951968f1f9c1f1f06e8f40807fe
gaykwadankit1 <126867933+gaykwadankit1@users.noreply.github.com>
2023-03-03 16:49:59 +0530

    Update A.java

--- Git/src/com/demo/A.java
+++ Git/src/com/demo/A.java
@@ -5,7 +5,11 @@ public class A
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello Ankit");
-		System.out.print("Hello ....Done");
+		System.out.println("Hello ....Done");
+		
+		System.out.println("Love121");
+	        System.out.println("Love121");
+
 		
 	}
 
