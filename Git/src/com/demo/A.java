beb5314295eea9d04ec00d2454e0786d6811cad5
HP <HP@DESKTOP-1A25VL2>
2023-03-03 16:57:03 +0530

    By Ap

--- Git/src/com/demo/A.java
+++ Git/src/com/demo/A.java
@@ -1,16 +1,13 @@
 package com.demo;
 
-public class A 
-{
-	public static void main(String[] args) 
-	{
+public class A {
+	public static void main(String[] args) {
 		System.out.println("Hello Ankit");
 		System.out.println("Hello ....Done");
-		
-		System.out.println("Love121");
-	        System.out.println("Love121");
 
-		
+		System.out.println("Love121");
+		System.out.println("Love121");
+
 	}
 
 }
